888f46a6db8270d48d949d33cd286ca10d9a982d
Josh Ellis <joshe523@gmail.com>
2020-03-05 03:42:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  def initialize(s1,s2,s3)
+    @sides = [s1,s2,s3]
+  end
+
+  def kind
+    if @sides[0] == @sides[1] && @sides[0] == @sides[2]
+      :equilateral
+    elsif @sides[0] == @sides[1] || @sides[0] == @sides[2] || @sides[1] == @sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
